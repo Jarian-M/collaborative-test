be428406a0e8e8ddee1b9fa6bcae8a1398d593c7
BT_C09_25 <mobileappdev@bths.edu>
2026-05-22 09:07:46 -0400

    Changed Cloud to Blue Cloud

--- collaborationfun/ContentView.swift
+++ collaborationfun/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "cloud")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Cloud")
+            Text("Blue Cloud")
         }
         .padding()
     }
