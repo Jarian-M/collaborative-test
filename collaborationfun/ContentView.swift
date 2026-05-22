a6370bcf5520fd3053c750dc28e5d14ac5be4459
BT_C09_25 <mobileappdev@bths.edu>
2026-05-22 09:04:07 -0400

    Changed emoji to cloud

--- collaborationfun/ContentView.swift
+++ collaborationfun/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "bolt")
+            Image(systemName: "cloud")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Thunda Struck")
